ce45578e8b2c8b0ed66155ead33f83ed1001069c
bvenceslas <bvenceslas@gmail.com>
2023-10-20 13:47:39 +0200

    add possibility to use impair count for the array

--- exo-algorithm/3-sort-array-by-pairs.js
+++ exo-algorithm/3-sort-array-by-pairs.js
@@ -1,14 +1,22 @@
-`given an array, sort its elements by pair
+/* 
+given an array, sort its elements by pair
 
-eg: arr = [12, 50, 20, 2, 1, 30, 5, 3, 5, 6, 7, 9, 3, 20] should be sorted
-and become [ 12, 50, 2, 20, 1, 30, 3, 5, 5, 6, 7, 9, 3, 20 ]
-`;
-const arr = [12, 50, 20, 2, 1, 30, 5, 3, 5, 6, 7, 9, 3, 20];
+ eg: arr = [12, 50, 20, 2, 1, 30, 5, 3, 5, 6, 7, 9, 3, 20] should be sorted
+ and become [ 12, 50, 2, 20, 1, 30, 3, 5, 5, 6, 7, 9, 3, 20 ]
+ */
+const arr = [12, 50, 20, 2, 1, 30, 5, 3, 5, 6, 7, 9, 3, 20, 5];
+
+arr.length;
 
 let pairs = [];
-for (let i = 0; i < arr.length - 1; i += 2) {
+for (let i = 0; i < arr.length; i += 2) {
   // assign sorted array
-  pairs.push(...[arr[i], arr[i + 1]].sort((a, b) => a - b));
+
+  if (!arr[i + 1]) {
+    pairs.push(...[arr[i]]);
+  } else {
+    pairs.push(...[arr[i], arr[i + 1]].sort((a, b) => a - b));
+  }
 }
 
 console.log(pairs);
